08858e30a66bdfc3e6bf8341e1a7c5991d554e5c
Mehdi Rezaei <mehdiraized@gmail.com>
2024-06-27 17:31:25 +0300

    add shadcn ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,62 +1,78 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./remotion/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  darkMode: "media",
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        "unfocused-border-color": "var(--unfocused-border-color)",
-        "focused-border-color": "var(--focused-border-color)",
-
-        "button-disabled-color": "var(--button-disabled-color)",
-        "disabled-text-color": "var(--disabled-text-color)",
-
-        "geist-error": "var(--geist-error)",
-
-        subtitle: "var(--subtitle)",
-      },
-      padding: {
-        "geist-quarter": "var(--geist-quarter-pad)",
-        "geist-half": "var(--geist-half-pad)",
-        geist: "var(--geist-pad)",
-      },
-      spacing: {
-        "geist-quarter": "var(--geist-quarter-pad)",
-        "geist-half": "var(--geist-half-pad)",
-        geist: "var(--geist-pad)",
-      },
-
-      borderRadius: {
-        geist: "var(--geist-border-radius)",
-      },
-
-      fontFamily: {
-        geist: "var(--geist-font)",
-      },
-
-      animation: {
-        spinner: "spinner 1.2s linear infinite",
-      },
-
-      keyframes: {
-        spinner: {
-          "0%": {
-            opacity: "1",
-          },
-          "100%": {
-            opacity: "0.15",
-          },
-        },
-      },
-    },
-  },
-  plugins: [],
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
+	],
+	prefix: "",
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			colors: {
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
